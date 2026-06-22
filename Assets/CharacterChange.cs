20bf6a77f50666c5e7a103b28b88624ee0ecefb8
Liberater3 <jovan3004@gmail.com>
2026-06-22 15:09:36 +0800

    :)

---
+++ Assets/CharacterChange.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class CharacterChange : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject cuttingBar;
+    public float fillRate = 0.5f;
+    public Animator animator;
+    public RuntimeAnimatorController runtimeAnimatorController;
+    public LayerMask layerMask;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f, layerMask))
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+            if(Input.GetKey(KeyCode.E))
+            {
+                animator.runtimeAnimatorController = runtimeAnimatorController;
+                cuttingBar.SetActive(true);
+                slider.value += fillRate * Time.deltaTime;
+            }
+            else
+            {
+                cuttingBar.SetActive(false);
+                animator.runtimeAnimatorController = null;
+            }
+        }
+    }
+}
