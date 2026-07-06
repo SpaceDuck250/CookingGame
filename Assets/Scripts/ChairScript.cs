93bc0879d7c6a2fd282b9e5ef3a8ec7d5e8f1d4d
SpaceDuck250 <alexthevlogger505@gmail.com>
2026-07-06 13:25:26 +0800

    betterCustomers

--- Assets/Scripts/ChairScript.cs
+++ Assets/Scripts/ChairScript.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class ChairScript : MonoBehaviour
@@ -8,13 +9,15 @@ public class ChairScript : MonoBehaviour
     public float seatTime;
 
     public Vector3 upOffset;
+    public float forwardOffsetValue;
 
     public GameObject tableParent;
 
     private void Start()
     {
         seatTime = 2;
-        upOffset = Vector3.up * 1.2f;
+        upOffset = Vector3.up * 0.9f;
+        forwardOffsetValue = 0.4f;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -33,8 +36,7 @@ public class ChairScript : MonoBehaviour
         {
             heldCustomer = movementScript;
             heldCustomer.sitting = true;
-            //CustomerSpawnerScript.OnCustomerSeated?.Invoke(heldCustomer);
-            print("go to exit sooner");
+
             SeatCustomer();
             Invoke("LeaveSeat", seatTime);
         }
@@ -59,7 +61,15 @@ public class ChairScript : MonoBehaviour
     {
         originalPosition = heldCustomer.transform.position;
         heldCustomer.agent.enabled = false;
-        heldCustomer.gameObject.transform.position = transform.position + upOffset;
+
+        Vector3 forwardOffset = (tableParent.transform.position - transform.position).normalized * forwardOffsetValue;
+
+        heldCustomer.gameObject.transform.position = transform.position + upOffset + forwardOffset;
+
+        Vector3 rotateVector = (tableParent.transform.position - transform.position).normalized;
+        float rotateAngle = Mathf.Atan2(rotateVector.x, rotateVector.z) * Mathf.Rad2Deg;
+
+        heldCustomer.gameObject.transform.rotation = Quaternion.Euler(0, rotateAngle, 0);
 
         heldCustomer.gameObject.GetComponent<CustomerAnimator>().Sit();
     }
