85c9669a500481656c1292cb4248601872040ef9
Liberater3 <jovan3004@gmail.com>
2026-07-07 17:04:05 +0800

    ok

--- Assets/PoopInteraction.cs
+++ Assets/PoopInteraction.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class PoopInteraction : Interactable
 {
     public GameObject broom;
+    public GameObject heldObject;
     void Update()
     {
         broom = GameObject.Find("Broom");
+        heldObject = GameObject.Find("HeldObjectContainer");
     }
     public override void Interact(PlayerHandScript playerHand)
     {
-        if(broom.activeSelf == true)
+        if(broom.transform.IsChildOf(heldObject.transform))
         {
             Destroy(gameObject);
         }
