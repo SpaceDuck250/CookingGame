4c82e604032150960dcfc06a22a3333f40d7a5d2
SpaceDuck250 <alexthevlogger505@gmail.com>
2026-07-13 17:26:07 +0800

    money stuff

--- Assets/Scripts/MoneyUIScript.cs
+++ Assets/Scripts/MoneyUIScript.cs
@@ -5,6 +5,11 @@ public class MoneyUIScript : MonoBehaviour
 {
     public TextMeshProUGUI moneyUIText;
 
+    public GameObject earnObj;
+    public TextMeshProUGUI earnTextComponent;
+
+    public GameObject tipObj;
+    public TextMeshProUGUI tipTextComponent;
 
     private void Start()
     {
@@ -16,9 +21,37 @@ public class MoneyUIScript : MonoBehaviour
         MoneyManager.OnMoneyChanged -= OnMoneyChanged;
     }
 
-    private void OnMoneyChanged(float moneyAmount)
+    private void OnMoneyChanged(float totalMoneyAmount, float earnedAmount, float tipAmount)
     {
-        moneyUIText.text = ": " + moneyAmount + "$";
+        moneyUIText.text = ": " + totalMoneyAmount + "$";
+
+        ShowEarnTextObj(earnedAmount, tipAmount);
+    }
+
+    private void ShowEarnTextObj(float amount, float tipAmount)
+    {
+        StopAllCoroutines();
+
+        earnTextComponent.text = "+" + amount + "$";
+
+        
+        tipTextComponent.text = "+" + tipAmount + "$ Tip";
+
+        earnObj.SetActive(true);
+
+        if (tipAmount > 0)
+        {
+            tipObj.SetActive(true);
+        }
+
+        float waitTime = 6f;
+        Invoke("HideBothEarnObj", waitTime);
+    }
+
+    public void HideBothEarnObj()
+    {
+        earnObj.SetActive(false);
+        tipObj.SetActive(false);
     }
 }
 
