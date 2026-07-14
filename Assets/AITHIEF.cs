f5cb6874560e4333d9f4bd90f61865fa7fb54b25
Liberater3 <jovan3004@gmail.com>
2026-07-14 16:30:29 +0800

    ok

--- Assets/AITHIEF.cs
+++ Assets/AITHIEF.cs
@@ -1,16 +1,52 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
-
-public class AITHIEF : MonoBehaviour
+using UnityEngine.AI;
+public class AITHIEF : Interactable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    NavMeshAgent thiefAgent;
+    public Transform thiefReady;
+    public GameObject dialogueText;
+    public TMP_Text textDialogue;
+    public Transform player;
+    bool readyToSteal;
+    Animator thiefAnimator;
     void Start()
     {
-        
+        thiefAgent = GetComponent<NavMeshAgent>();
+        thiefAgent.SetDestination(thiefReady.position);
+        thiefAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(player.position, transform.position);
+        if(distance > 5)
+        {
+            dialogueText.SetActive(false);
+            textDialogue.text = "";
+        }
+         if(!thiefAgent.pathPending &&
+        thiefAgent.remainingDistance <= thiefAgent.stoppingDistance && (!thiefAgent.hasPath || thiefAgent.velocity.sqrMagnitude <= 0.5f))
+        {
+            thiefAgent.ResetPath();
+            thiefAgent.isStopped = true;
+            thiefAnimator.SetBool("Idle", true);
+        }
+    }
+    void Escape()
+    {
         
+    }
+    public override void Interact(PlayerHandScript playerHand)
+    {
+        Debug.Log("HOLE IN ONE");
+        if(!thiefAgent.pathPending &&
+        thiefAgent.remainingDistance <= thiefAgent.stoppingDistance && (!thiefAgent.hasPath || thiefAgent.velocity.sqrMagnitude <= 0.5f))
+        {
+            dialogueText.SetActive(true);
+            textDialogue.text = "I'll like your mom please";
+        }
     }
 }
