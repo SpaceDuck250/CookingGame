a5390da6d6702dd4e1dbbd29ed4f84e7c56ca61d
TinMan <tintin.mages@gmail.com>
2026-07-17 13:59:49 +0800

    HawkerTesting

---
+++ Assets/Scripts/TintinAIScripts/DoorScript.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class DoorScript : Interactable
+{
+    public bool isOpen = false;
+
+    public Transform doorTransform;
+
+    public float openAngle = 90f;
+    public float rotateSpeed = 3f;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    private Quaternion targetRotation;
+
+    private void Awake()
+    {
+        closedRotation = doorTransform.localRotation;
+        openRotation = Quaternion.Euler(0, openAngle, 0) * closedRotation;
+
+        targetRotation = closedRotation;
+    }
+
+    private void Update()
+    {
+        doorTransform.localRotation = Quaternion.RotateTowards(doorTransform.localRotation, targetRotation, rotateSpeed * 100f * Time.deltaTime);
+    }
+
+    public override void Interact(PlayerHandScript playerHand)
+    {
+        ToggleDoor();
+    }
+
+    public void ToggleDoor()
+    {
+        isOpen = !isOpen;
+        targetRotation = isOpen ? openRotation : closedRotation;
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+        targetRotation = openRotation;
+    }
+
+    public void CloseDoor()
+    {
+        isOpen = false;
+        targetRotation = closedRotation;
+    }
+}
