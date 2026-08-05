fe7463d2a9e14b35589d4d705b8c2bbf9bfd608f
TinMan <tintin.mages@gmail.com>
2026-08-05 10:59:15 +0800

    Fix

--- Assets/Scripts/CarrotCutter.cs
+++ Assets/Scripts/CarrotCutter.cs
@@ -9,7 +9,6 @@ public class CarrotCutter : Interactable
     public FoodData cutFoodData;
 
     public CookingInputOutputScript cookingInputOutput;
-    public event Action<int, int> OnChopped;
 
     public Transform foodSpawnArea;
     public Vector3 spawnOffset;
