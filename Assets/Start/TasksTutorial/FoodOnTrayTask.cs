aa2a23df0997c146f438e5f2402d8873aa382609
Liberater3 <jovan3004@gmail.com>
2026-09-08 15:54:24 +0800

    ok

--- Assets/Start/TasksTutorial/FoodOnTrayTask.cs
+++ Assets/Start/TasksTutorial/FoodOnTrayTask.cs
@@ -6,6 +6,8 @@ public class FoodOnTrayTask : TutorialTask
 
     public FoodData correctFoodData;
 
+    public GameObject laptop;
+
     //public HoldableFoodScript holdableFoodScript;
 
     private void Start()
@@ -27,6 +29,7 @@ public class FoodOnTrayTask : TutorialTask
 
         //holdableFoodScript.enabled = true;
         CompleteTask();
+        laptop.layer = LayerMask.GetMask("Clickable");
     }
 
 }
