12d8dc080a3718e721f6f0b0971b709dd4e9534c
SpaceDuck250 <alexthevlogger505@gmail.com>
2026-09-15 10:25:29 +0800

    customers feel less dumb

--- Assets/Scripts/StupidDumbassCustomer.cs
+++ Assets/Scripts/StupidDumbassCustomer.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 
 public class StupidDumbassCustomer : MonoBehaviour
 {
-    public float waitTime;
+    public float waitTimeOffset;
+
+    [SerializeField]
+    private float waitTime;
+
+
+    private float extraRandomTime;
+
+
     public float timer;
 
     public CustomerMovementScript moveScript;
@@ -12,16 +20,29 @@ public class StupidDumbassCustomer : MonoBehaviour
 
     public float iq;
 
+    private void Start()
+    {
+        extraRandomTime = GetNewOffsetAddedTime();
+    }
+
+
     private void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= waitTime)
+        if (timer >= waitTime + extraRandomTime)
         {
             int randomIndex = Random.Range(0, wanderPointList.Count);
             Transform newDestination = wanderPointList[randomIndex];
             moveScript.OnNewDestinationChange(newDestination);
 
+            extraRandomTime = GetNewOffsetAddedTime();
+
             timer = 0;
         }
+    }
+
+    private float GetNewOffsetAddedTime()
+    {
+        return Random.Range(-waitTimeOffset, waitTimeOffset);
     }
 }
